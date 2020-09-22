8069928b96a6faf84cf7be5f44224baaca3f7c65
Adam Bullard <adambsimple@gmail.com>
2020-09-22 14:15:55 -0700

    UI progress

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { TouchableOpacity, AsyncStorage, StyleSheet, Text, Image, Button, View, ActivityIndicator, StatusBar, Dimensions } from 'react-native';
 import LinkedInModal from 'react-native-linkedin';
-import Storage from './localstorage';
+// import Storage from './localstorage';
 
 const Stack = createStackNavigator();
 
@@ -13,8 +13,8 @@ const Stack = createStackNavigator();
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 const windowHeight6 = windowHeight / 6;
-const titleWidth = windowWidth - 100;
-const mainButtonWidth = windowWidth - 50;
+const mainWidth = windowWidth - 50;
+const mainConversationWidth = windowWidth - 120;
 
 const styles = StyleSheet.create({
   container: {
@@ -22,19 +22,69 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+
+  MentorBox: {
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: '#006B3F',
+    padding: 1,
+    backgroundColor: '#338965'
+  },
+
+  MentorTag: {
+    textAlign: 'center',
+    color:'white',
+    padding:3,
+    fontSize: 12
   }
 })
 
+
 // Functions for generating various pages below.
 
+// Get necessary data for HomeScreen.
+
+// accountType: 0 - not verified, please wait until admins pair you with mentor/mentees
+//              1 - verified, check for conversations to display
+// const accountType = Storage.getItem('accountType'); 
+const accountType = 0;
 const HomeScreen = ({ navigation }) => {
   return (
       <View style={{flex: 1, flexDirection: 'column'}}>
-        <View style={{height:10, width:windowWidth}} />
-        <View style={{width: windowWidth, flexDirection: 'row-reverse', alignItems:'center'}}>
-          <View style={{width: 25}} />
-          <Button style={{width: mainButtonWidth, height: 60, backgroundColor: 'blue'}} />
+        {accountType == 1 ? [
+          <View style={{height:50, width:windowWidth}} />,
+          <View style={{width: windowWidth, flexDirection: 'row-reverse', alignItems:'center'}}>
+            <View style={{width: 25}} />
+            <View style={{width: mainWidth, alignItems:'center', justifyContent:'center'}}>
+              <Text style={{textAlign:'center', fontSize:22}}>Welcome to the CSWWU Mentors!</Text>
+              <View style={{height: 25}} />
+              <Text style={{textAlign:'center', fontSize:22}}>Admins are verifying your profile, check back later to be connected with your mentor/mentee.</Text>
+          </View>
         </View>
+        ] : [
+          <View style={{width:windowWidth, height:100, flexDirection:'row', alignItems:'center', backgroundColor:'white'}}>
+            <View style={{width:80, alignItems:'center', justifyContent:'center'}}>
+              <Image style={{width:60, height:60}} source={require('./assets/avatar.png')} />
+            </View>
+            <View style={{width: mainConversationWidth, flexDirection:'column'}}>
+              <View style={{flexDirection:'row'}}>
+                <Text style={{fontSize:20}}>John Smith</Text>
+                <View style={{width:5}} />
+                <View style={styles.MentorBox}>
+                  <Text style={styles.MentorTag}>Mentor</Text>
+                </View>
+              </View>
+              <View style={{height:4}} />
+              <View>
+                <Text>This is a previous of our conversation. I'll...</Text>
+              </View>
+            </View>
+            <View style={{width:40, alignItems:'center', justifyContent:'center'}}>
+              <Image style={{width:24, height:24}} source={require('./assets/right-message-chevron.png')} />
+            </View>
+          </View>
+        ]}
       </View>
   );
 };

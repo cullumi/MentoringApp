eb0a7413f97a67623c73e832ed0c11cd862aaf40
Ian Cullum <cullumi@wwu.edu>
2021-01-12 03:13:21 -0800

    Content Complete: Contact Info Screen
    + ContactInfoScreen complete.
    + getContactInfoOf() method added; no errors caught.
    + Duplicated ContactInfoScreen code over to the ProposeMeetingScreen class as a placeholder. (You can now navigate to the ProposeMeetingScreen via a very ugly button the ContactInfoScreen)

--- App.js
+++ App.js
@@ -793,7 +793,7 @@ class HomeScreen extends React.Component {
           </View>
           <View style={styles.homeItemForward}>
             <IonIcon type='Ionicons' name='ios-arrow-forward' size={30} color='#000000' onPress={() =>
-              this.props.navigation.navigate('ContactInfo', { id: otherUser.Id })} />
+              this.props.navigation.navigate('ContactInfo', { user: otherUser, type: otherType })} />
               {/* Use this to pass data to your next screen.
                 Check out lines 1157 and 1315 for an example of passing data. */ }
           </View>
@@ -814,24 +814,119 @@ class HomeScreen extends React.Component {
 }
 
 
+async function getContactInfoOf(userID) {
+  
+  const cInfores = await fetch(url + '/contact/' + userID, {
+    method: 'GET'
+  });
+
+  const cInfoPayload = await cInfores.json();
+
+  const recordSet = cInfoPayload["recordset"];
+  var cInfos = [];
+  for (var i = 0; i < recordSet.length; i++) {
+    var cInfo = JSON.parse(JSON.stringify(recordSet[i]));
+    cInfo.LastUpdateText = parseSimpleDateText(new Date(topic.LastUpdate));
+    cInfo.CreatedText = parseSimpleDateText(new Date(topic.Created));
+    cInfos.push(cInfo);
+  }
+
+  return cInfos;
+}
+
 class ContactInfoScreen extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      refreshing : false
+      refreshing : true,
+      contactInfo : []
     };
   }
 
-  displayCI() {
-    return(
+  async setContactInfo() {
+    var newCI = [];
+    var doSetAsyncStorage = false;
+    const userID = this.props.route.params.user.Id;
+    // console.warn(userID);
 
+    try {
+      newCI = await getContactInfoOf(userID);
+      doSetAsyncStorage = true;
+
+    } catch (error) {
+      console.log(error);
+      try {
+        var tempCI = JSON.parse(await AsyncStorage.getItem('ContactInfo/' + userID));
+        if (tempCI != null && Array.isArray(tempCI)) {
+          newCI = tempCI;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    // Save mentor/mentee info from the database into local storage, for when you're offline.
+    if (doSetAsyncStorage) {
+      try {
+        await AsyncStorage.setItem('ContactInfo/' + userID, JSON.stringify(newCI));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    this.setState({refreshing: false, contactInfo: newCI});
+  }
+
+  infoItem(info) {
+    return (
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <Text>{ info.ContactType }</Text>
+        <Text>{ info.ContactValue }</Text>
+      </View>
+    );
+  }
+
+  displayCI(cInfo) {
+
+    const user = this.props.route.params.user;
+    const type = this.props.route.params.type;
+
+    return(
+      <View style={{justifyContent: 'flex-end', alignItems: 'center',paddingTop:25}}>
+          <View style={{flexGrow: 1}}>
+              <Image style={styles.homeItemAvatar} source={{uri: user.Avatar}} />
+              <View style={user.homeBoxStyle}>
+                <Text style={styles.homeTag}>{ type } </Text>
+              </View>
+          </View>
+          <View>
+              <Text>{ user.FirstName + " " + user.LastName }</Text>
+              { console.log(cInfo) }
+              { cInfo.map( (info) => {
+                  return this.infoItem(info); 
+              })}
+          </View>
+          <Button onPress={ () => this.props.navigation.navigate('ProposeMeeting', { user: user, type: type })}>
+              <View style={user.homeBoxStyle}>
+                  <Text style={styles.homeTag}>Propose Meeting</Text>
+              </View>
+          </Button>
+      </View>
     );
   }
 
   render() {
+
+    if (this.state.refreshing) {
+      this.setContactInfo()
+    }
+
+    console.log("Rendering Contact Info Screen...");
+
     return (
       <View style={{flex: 1, flexDirection: 'column'}}>
         { backTitleBar("Contact Info", () => this.props.navigation.navigate('SettingsModal'), this.props.navigation) }
+        { this.displayCI(this.state.contactInfo) }
       </View>
     );
   }
@@ -1559,12 +1654,97 @@ class ProposeMeetingScreen extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      refreshing : false
+      refreshing : true,
+      contactInfo : []
     };
   }
 
+  async setContactInfo() {
+    var newCI = [];
+    var doSetAsyncStorage = false;
+    const userID = this.props.route.params.user.Id;
+    // console.warn(userID);
+
+    try {
+      newCI = await getContactInfoOf(userID);
+      doSetAsyncStorage = true;
+
+    } catch (error) {
+      console.log(error);
+      try {
+        var tempCI = JSON.parse(await AsyncStorage.getItem('ContactInfo/' + userID));
+        if (tempCI != null && Array.isArray(tempCI)) {
+          newCI = tempCI;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    // Save mentor/mentee info from the database into local storage, for when you're offline.
+    if (doSetAsyncStorage) {
+      try {
+        await AsyncStorage.setItem('ContactInfo/' + userID, JSON.stringify(newCI));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    this.setState({refreshing: false, contactInfo: newCI});
+  }
+
+  infoItem(info) {
+    return (
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <Text>{ info.ContactType }</Text>
+        <Text>{ info.ContactValue }</Text>
+      </View>
+    );
+  }
+
+  displayCI(cInfo) {
+
+    const user = this.props.route.params.user;
+    const type = this.props.route.params.type;
+
+    return(
+      <View style={{justifyContent: 'flex-end', alignItems: 'center',paddingTop:25}}>
+          <View style={{flexGrow: 1}}>
+              <Image style={styles.homeItemAvatar} source={{uri: user.Avatar}} />
+              <View style={user.homeBoxStyle}>
+                <Text style={styles.homeTag}>{ type } </Text>
+              </View>
+          </View>
+          <View>
+              <Text>{ user.FirstName + " " + user.LastName }</Text>
+              { console.log(cInfo) }
+              { cInfo.map( (info) => {
+                  return this.infoItem(info); 
+              })}
+          </View>
+          <Button onPress={ () => this.props.navigation.navigate('ProposeMeeting', { user: user, type: type })}>
+              <View style={user.homeBoxStyle}>
+                  <Text style={styles.homeTag}>Propose Meeting</Text>
+              </View>
+          </Button>
+      </View>
+    );
+  }
+
   render() {
-    //
+
+    if (this.state.refreshing) {
+      this.setContactInfo()
+    }
+
+    console.log("Rendering Propose Meeting Screen...");
+
+    return (
+      <View style={{flex: 1, flexDirection: 'column'}}>
+        { backTitleBar("Propose Meeting", () => this.props.navigation.navigate('SettingsModal'), this.props.navigation) }
+        { this.displayCI(this.state.contactInfo) }
+      </View>
+    );
   }
 }
 

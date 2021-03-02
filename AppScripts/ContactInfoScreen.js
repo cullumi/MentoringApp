bd9b2717e853618650ad03968b7b1c1b5073ba7b
bullara2 <bullara2@wwu.edu>
2021-03-01 21:50:03 -0800

    implemented mailto: and create contact methods for ContactInfoScreen

--- AppScripts/ContactInfoScreen.js
+++ AppScripts/ContactInfoScreen.js
@@ -3,11 +3,12 @@
 
 
 import React from 'react';
-import {View, TouchableOpacity, Text, Button, Image, AsyncStorage} from 'react-native';
+import { Linking, View, TouchableOpacity, Text, Button, Image, AsyncStorage} from 'react-native';
 import IonIcon from 'react-native-vector-icons/Ionicons';
 import {BackTitleBarContact} from './ScreenComponents.js';
 import {styles, colors} from './Styles.js';
 import {getContactInfoOf} from './API.js';
+import * as Contacts from 'expo-contacts';
 
 export default class ContactInfoScreen extends React.Component {
     constructor(props) {
@@ -17,13 +18,13 @@ export default class ContactInfoScreen extends React.Component {
         contactInfo : []
       };
     }
-  
+
     componentDidMount() {
       if (this.state.refreshing) {
         this.setContactInfo()
       }
     }
-  
+
     async setContactInfo() {
 
       console.log("setting contact info...");
@@ -32,13 +33,13 @@ export default class ContactInfoScreen extends React.Component {
       var doSetAsyncStorage = false;
       const userID = this.props.route.params.user.Id;
       // console.warn(userID);
-  
+
       console.log('uid: ' + userID);
-  
+
       try {
         newCI = await getContactInfoOf(userID);
         doSetAsyncStorage = true;
-  
+
       } catch (error) {
         console.log(error);
         try {
@@ -50,7 +51,7 @@ export default class ContactInfoScreen extends React.Component {
           console.log(error);
         }
       }
-  
+
       // Save mentor/mentee info from the database into local storage, for when you're offline.
       if (doSetAsyncStorage) {
         try {
@@ -59,7 +60,7 @@ export default class ContactInfoScreen extends React.Component {
           console.log(error);
         }
       }
-  
+
       newCI.map( (info) => {
           switch(info.ContactType) {
             case 'Email':
@@ -70,29 +71,99 @@ export default class ContactInfoScreen extends React.Component {
             break;
           }
       })
-  
+
       this.setState({refreshing: false, contactInfo: newCI});
     }
-  
+
+    async openContactPicker() {
+
+      const { status } = await Contacts.requestPermissionsAsync();
+
+      if (status == 'granted') {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.PhoneNumbers],
+        });
+
+        if (data.length > 0) {
+          const contact = data[1];
+          console.log(contact);
+        }
+
+        if (data.length > 0) {
+          const contact = data[0];
+          console.log(contact);
+        }
+        var ci = this.state.contactInfo;
+        const user = this.props.route.params.user;
+        var givenName = user.FirstName;
+        var familyName = user.LastName;
+
+        var email = '';
+        var phone = '';
+
+        ci.map((info) => {
+          switch(info.ContactType) {
+            case 'Email':
+            email = info.ContactValue;
+            break;
+            case 'Phone':
+            phone = info.ContactValue;
+            break;
+          }
+        });
+
+        phone.replace("(","");
+        phone.replace(")","");
+        phone.replace("-","");
+        phone.replace(" ","");
+        phone = '+1' + phone;
+        const contact = {
+          [Contacts.Fields.FirstName]: givenName,
+          [Contacts.Fields.LastName]: familyName,
+          [Contacts.Fields.PhoneNumbers]: [{digits:phone,number:phone,countryCode:'us'}],
+          [Contacts.Fields.Emails]: [{email:email}],
+        };
+
+        const contactId = await Contacts.addContactAsync(contact);
+        await Contacts.presentFormAsync(contactId);
+
+      }
+
+    };
+
     infoItem(info) {
-      return (<View>
-        <TouchableOpacity style={styles.contactRow}>
-          <View style={styles.contactIconContainer}>
-            <IonIcon type='Ionicons' name={info.ContactIcon} size={30} color={colors.vikingBlue} />
-          </View>
-          <View style={styles.contactRowText}>
-            <Text style={styles.contactRowType}>{ info.ContactType }</Text>
-            <Text style={styles.contactRowValue}>{ info.ContactValue }</Text>
-          </View>
-        </TouchableOpacity>
-      </View>);
+      if (info.ContactType == 'Email') {
+        return (<View>
+          <TouchableOpacity style={styles.contactRow} onPress={() => Linking.openURL('mailto:' + info.ContactValue)}>
+            <View style={styles.contactIconContainer}>
+              <IonIcon type='Ionicons' name={info.ContactIcon} size={30} color={colors.vikingBlue} />
+            </View>
+            <View style={styles.contactRowText}>
+              <Text style={styles.contactRowType}>{ info.ContactType }</Text>
+              <Text style={styles.contactRowValue}>{ info.ContactValue }</Text>
+            </View>
+          </TouchableOpacity>
+        </View>);
+      } else {
+        return (<View>
+          <TouchableOpacity style={styles.contactRow} onPress={() => this.openContactPicker()}>
+            <View style={styles.contactIconContainer}>
+              <IonIcon type='Ionicons' name={info.ContactIcon} size={30} color={colors.vikingBlue} />
+            </View>
+            <View style={styles.contactRowText}>
+              <Text style={styles.contactRowType}>{ info.ContactType }</Text>
+              <Text style={styles.contactRowValue}>{ info.ContactValue }</Text>
+            </View>
+          </TouchableOpacity>
+        </View>);
+      }
     }
-  
+
     displayCI(cInfo) {
-  
+
       const user = this.props.route.params.user;
       const type = this.props.route.params.type;
-  
+
       return(
         <View style={styles.contactContainer}>
             <View style={{flexGrow: 1}}>
@@ -117,11 +188,11 @@ export default class ContactInfoScreen extends React.Component {
         </View>
       );
     }
-  
+
     render() {
-  
+
       console.log("Rendering Contact Info Screen...");
-  
+
       return (
         <View style={{flex: 1, flexDirection: 'column', backgroundColor:'#fff'}}>
           <BackTitleBarContact title="Contact Info" navigation={this.props.navigation} />
